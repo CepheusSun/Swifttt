11a8d85156a418fdd8226fbbf1b9324396418e67
CepheusSun <cd_sunyang@163.com>
2017-03-22 14:50:20 +0800

    添加首页model

---
+++ SwiftLive/SwiftLive/Class/Home/Model/HomeCellModel.swift
@@ -0,0 +1,24 @@
+//
+//  HomeCellModel.swift
+//  SwiftLive
+//
+//  Created by sunny on 2017/3/22.
+//  Copyright © 2017年 CepheusSun. All rights reserved.
+//
+
+import Foundation
+
+struct HomeCellModel {
+    
+    /// 头像
+    var portrait = ""
+    /// 主播昵称
+    var nick = ""
+    /// 主播地址
+    var location = ""
+    /// 观看人数
+    var viewers = 0
+    /// 直播流的地址
+    var url = ""
+    
+}
