a1111a505aee23682f0fb289211f4df5a3950bf7
CepheusSun <cd_sunyang@163.com>
2017-03-22 16:38:12 +0800

    添加ijk 必要的库

---
+++ SwiftLive/SwiftLive/Class/Live/Controller/LiveViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LiveViewController.swift
+//  SwiftLive
+//
+//  Created by sunny on 2017/3/22.
+//  Copyright © 2017年 CepheusSun. All rights reserved.
+//
+
+import UIKit
+
+class LiveViewController: UIViewController {
+
+    var model: HomeModel!
+    var playerView: UIView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
